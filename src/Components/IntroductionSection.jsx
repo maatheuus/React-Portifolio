283a9375b2220f873e6326157c0d77ec51b107a7
MATHEUS <matts14smkd@gmail.com>
2024-02-26 18:52:57 -0300

    estilizando section

--- src/Components/IntroductionSection.jsx
+++ src/Components/IntroductionSection.jsx
@@ -9,38 +9,60 @@ export default function IntroductionSection() {
         <div className="content-section__introduction">
           <h1 className="content-section__introduction-title">
             <span className="span-hi">Hi, I am </span>
-            <span className="span-name">Maat Bakari</span>
+            <span>Maat Bakari.</span>
           </h1>
           <p className="content-section__introduction-text">
-            Front-end developer passionate building accessible and user friendli
+            Front-end developer passionate building accessible and user friendly
             websites.
           </p>
           <div className="section-list">
             <ul className="section-list__buttons">
               <li>
-                <Button className="button section-list__buttons--contact">
-                  <span>Contact me</span>
-                  <div className="arrow">
-                    <ArrowLink className="arrow-link" />
-                  </div>
-                </Button>
+                <a
+                  href="mailto:matts14smkd@gmail.com"
+                  target="_blanc"
+                  className="link-style"
+                >
+                  <Button className="button section-list__buttons--contact">
+                    <span>Contact me</span>
+                    <div className="arrow">
+                      <ArrowLink className="arrow-link" />
+                    </div>
+                  </Button>
+                </a>
               </li>
               <li>
-                <Button className="button section-list__buttons--linkedin">
-                  {" "}
-                  <FaLinkedin />
-                </Button>
+                <a
+                  href="https://www.linkedin.com/in/matheusmaat/"
+                  target="_blanc"
+                  className="link-style"
+                >
+                  <Button className="button section-list__buttons--linkedin">
+                    {" "}
+                    <FaLinkedin className="svg" />
+                  </Button>
+                </a>
               </li>
               <li>
-                <Button className="button section-list__buttons--gitHub">
-                  {" "}
-                  <FaGithub />
-                </Button>
+                <a
+                  href="https://github.com/maatheuus"
+                  target="_blanc"
+                  className="link-style"
+                >
+                  <Button className="button section-list__buttons--github">
+                    {" "}
+                    <FaGithub className="svg" />
+                  </Button>
+                </a>
               </li>
             </ul>
           </div>
         </div>
-        <div className="content-section__profile">PROFILE</div>
+        <div className="content-section__profile">
+          <div className="content-section__profile--image">
+            {/* <img src="" alt="photo of the user" /> */}
+          </div>
+        </div>
       </div>
     </section>
   );
